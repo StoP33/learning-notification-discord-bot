afc64c108653154cff4beded5f8028677665cc13
tp2012 <53143403+tp2012@users.noreply.github.com>
2020-04-03 18:27:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const notification = require('./modules/notification');
 
-const username = 'ITITIU19180';
-const password = 'Findtheaim2020';
-const token = 'Njk0OTE0OTM0ODA2NTQ0Mzk1.XocdDQ.81XR8Edu9L-Nm3y8nOd6Nh90YNY';
+const username = process.env.ID;
+const password = process.env.PASSWORD;
+const token = process.env.TOKEN;
 
 const client = new Discord.Client();
 
